6851392334faccbb0c8b1c9c92fce8daa36be4fd
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-06-20 16:10:04 -0400

    Initial commit

---
+++ problems/09-call-on-target.js
@@ -0,0 +1,8 @@
+function callOnTarget(func, obj1, obj2) {
+  // Your code here
+}
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+module.exports = callOnTarget;
